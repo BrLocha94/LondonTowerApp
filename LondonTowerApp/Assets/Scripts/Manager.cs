f38499fe89c9b411d685d12afde29f5262b74b96
BrLocha94 <brunolocha2@gmail.com>
2019-10-17 02:33:18 -0300

    Updated project to load scenes from coroutine

--- LondonTowerApp/Assets/Scripts/Manager.cs
+++ LondonTowerApp/Assets/Scripts/Manager.cs
@@ -6,11 +6,25 @@ public class Manager : MonoBehaviour
 {
     SoundController soundController;
     PlayerData playerData;
+    SceneController sceneController;
 
 	void Start ()
     {
         soundController = SoundController.instance();
         playerData = PlayerData.instance();
+        sceneController = SceneController.instance();
 	}
-	
+    
+    public void changeScene(string param)
+    {
+        if (param.Equals("Game"))
+            sceneController.loadGame();
+        else if (param.Equals("Open"))
+            sceneController.loadOpen();
+        else if (param.Equals("Results"))
+            sceneController.loadResults();
+        else if (param.Equals("Tutorial"))
+            sceneController.loadTutorial();
+    }
+
 }
--- LondonTowerApp/Assets/Scripts/SceneController.cs
+++ LondonTowerApp/Assets/Scripts/SceneController.cs
@@ -5,23 +5,74 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
+    private static SceneController _instance;
+    public static SceneController instance()
+    {
+        if (_instance != null)
+            return _instance;
+
+        _instance = GameObject.FindObjectOfType<SceneController>();
+
+        if (_instance == null)
+        {
+            GameObject instanceResource = Resources.Load("SceneController") as GameObject;
+
+            if (instanceResource != null)
+            {
+                GameObject instanceObject = Instantiate(instanceResource);
+                _instance = instanceObject.GetComponent<SceneController>();
+                DontDestroyOnLoad(instanceObject);
+            }
+        }
+
+        return _instance;
+    }
+
     public void loadOpen()
     {
+        StartCoroutine(loadOpenRoutine());
+    }
+
+    IEnumerator loadOpenRoutine()
+    {
+        yield return new WaitForSeconds(0.2f);
+
         SceneManager.LoadScene("Open", LoadSceneMode.Single);
     }
 
     public void loadTutorial()
     {
+        StartCoroutine(loadTutorialRoutine());
+    }
+
+    IEnumerator loadTutorialRoutine()
+    {
+        yield return new WaitForSeconds(0.2f);
+
         SceneManager.LoadScene("Tutorial", LoadSceneMode.Single);
     }
 
     public void loadResults()
     {
+        StartCoroutine(loadResultsRoutine());
+    }
+
+    IEnumerator loadResultsRoutine()
+    {
+        yield return new WaitForSeconds(0.2f);
+
         SceneManager.LoadScene("Results", LoadSceneMode.Single);
     }
 
     public void loadGame()
     {
+        StartCoroutine(loadGameRoutine());
+    }
+
+    IEnumerator loadGameRoutine()
+    {
+        yield return new WaitForSeconds(0.2f);
+
         SceneManager.LoadScene("Game", LoadSceneMode.Single);
     }
 }
