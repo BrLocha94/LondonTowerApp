6f75f7b05c1ca636a96632db863f4687ce32730e
BrLocha94 <brunolocha2@gmail.com>
2019-10-18 11:05:46 -0300

    Updated scenes structure and added new scripts to the project. Tutorial base is finished

---
+++ LondonTowerApp/Assets/Scripts/TutorialController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TutorialController : MonoBehaviour
+{
+    public Manager manager;
+
+    int current_step = 0;
+
+    [SerializeField]
+    Image[] tutorial_steps;
+
+	public void nextStep()
+    {
+        if(current_step >= tutorial_steps.Length - 1)
+        {
+            if (manager != null)
+                manager.changeScene("Open");
+            else
+                Debug.Log("Manager not found");
+        }
+        else
+        {
+            tutorial_steps[current_step].gameObject.SetActive(false);
+            current_step++;
+            tutorial_steps[current_step].gameObject.SetActive(true);
+        }
+    }
+
+    public void backStep()
+    {
+        if(current_step >= 0)
+        {
+            tutorial_steps[current_step].gameObject.SetActive(false);
+            current_step--;
+            tutorial_steps[current_step].gameObject.SetActive(true);
+        }
+    }
+}
