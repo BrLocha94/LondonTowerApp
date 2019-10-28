b55642b0ed6dcd3954c8c5d12ac5c4212ce851f4
BrLocha94 <brunolocha2@gmail.com>
2019-10-28 17:30:28 -0300

    Updated sound system (is now working)

--- LondonTowerApp/Assets/Scripts/_instancesScripts/SoundController.cs
+++ LondonTowerApp/Assets/Scripts/_instancesScripts/SoundController.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class SoundController : MonoBehaviour
 {
-    bool is_enabled = false;
+    bool is_enabled = true;
+
+    [SerializeField]
+    private AudioSource background;
+    [SerializeField]
+    private AudioSource button;
+    [SerializeField]
+    private AudioSource sfx;
 
     private static SoundController _instance;
     public static SoundController instance()
@@ -39,8 +46,40 @@ public class SoundController : MonoBehaviour
         _instance.is_enabled = !_instance.is_enabled;
 
         if (_instance.is_enabled == true)
+        {
+            _instance.background.mute = true;
+            _instance.button.mute = true;
+            _instance.sfx.mute = true;
             Debug.Log("Sound ON");
+        }
         else
+        {
+            _instance.background.mute = false;
+            _instance.button.mute = false;
+            _instance.sfx.mute = false;
             Debug.Log("Sound OFF");
+        }
+    }
+
+    public void playBackground()
+    {
+        if(_instance.is_enabled == true)
+            _instance.background.Play();
+    }
+
+    public void playButton()
+    {
+        Debug.Log("AQUI");
+        if (_instance.is_enabled == true)
+            _instance.button.Play();
+    }
+
+    public void playSfx(AudioClip param)
+    {
+        if (_instance.is_enabled == true)
+        {
+            _instance.sfx.clip = param;
+            _instance.sfx.Play();
+        }
     }
 }
