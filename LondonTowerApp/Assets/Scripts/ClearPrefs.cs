17fe59e52a8479cdde6a468d398376794fa76a82
BrLocha94 <brunolocha2@gmail.com>
2019-11-13 00:29:11 -0300

    Created delete player prefs scenes

---
+++ LondonTowerApp/Assets/Scripts/ClearPrefs.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearPrefs : MonoBehaviour
+{
+
+	void Start ()
+    {
+        PlayerPrefs.DeleteAll();
+	}
+	
+	
+}
